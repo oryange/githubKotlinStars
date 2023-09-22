8a045d351952f03d281714bf524145a8d1efbadf
oryange <oryangestrifezze@gmail.com>
2023-09-21 21:33:50 -0300

    Feat: config layout repositoryList

--- app/src/main/java/com/example/app/view/RepositoryList.kt
+++ app/src/main/java/com/example/app/view/RepositoryList.kt
@@ -1,8 +1,30 @@
 package com.example.app.view
 
-import androidx.compose.material.Text
+import android.annotation.SuppressLint
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.app.ui.theme.Purple700
+import com.example.app.ui.theme.White
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun RepositoryList(navController: NavController) {}
+fun RepositoryList(navController: NavController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                backgroundColor = Purple700,
+                title = {
+                    Text(
+                        text = "Github Kotlin Stars",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = White
+                    )
+                }
+            )
+        }
+    ) {}
+}
