02ff757b0c1005157cccd2c163ccc3a90646c147
oryange <oryangestrifezze@gmail.com>
2023-09-22 15:35:38 -0300

    Feat: config retrofit II

---
+++ app/src/main/java/com/example/app/service/response/RepositoryResponse.kt
@@ -0,0 +1,10 @@
+package com.example.app.service.response
+
+import com.google.gson.annotations.SerializedName
+
+data class RepositoryResponse(
+    @SerializedName("name") val name: String,
+    @SerializedName("stargazers_count") val stargazersCount: Int,
+    @SerializedName("forks_count") val forksCount: Int,
+    @SerializedName("owner") val owner: Owner
+)
